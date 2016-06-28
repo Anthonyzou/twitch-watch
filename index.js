ac4e69d869e219df224b5a1e7158835231eae119
Anthony Ou <azou@ualberta.ca>
2016-06-28 22:54:29 +0000

    this seems to work

--- index.js
+++ index.js
@@ -5,24 +5,30 @@ const _ = require('lodash');
 const rp = require('request-promise');
 const random_useragent = require('random-useragent');
 
-rp('http://gimmeproxy.com/api/getProxy')
+rp('http://gimmeproxy.com/api/getProxy',{
+  qs : {
+    'protocol' : 'http',
+    "user-agent" : true,
+  }
+})
 .then(result => {
-  console.log(result)
+  result = JSON.parse(result)
 
   const nightmare = Nightmare({
-    show: true,
+    show: false,
     webPreferences: {
       plugins: true
     },
     switches : {
       'ppapi-flash-path': process.platform === 'darwin' ? './PepperFlashPlayer.plugin' : './libpepflashplayer.so',
-      'proxy-server': result.ipPort,
+      'proxy-server': `${result.type}://${result.ipPort}`,
+      'ignore-certificate-errors': true
     }
   });
-  
+
   const page = nightmare
-    .useragent(random_useragent.getRandom().userAgent)
-    .goto('http://www.twitch.tv/malothekilla')
+    // .useragent(random_useragent.getRandom(ua => ua.browserName == 'Chrome'))
+    .goto('http://www.twitch.tv/jbozzz')
     .on('page', console.log.bind(console))
 
     .run((err, nightmare) => {
