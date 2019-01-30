61869e11dd7d9559012377d366f17f492bff92cf
Anthony Ou <ou.anthony@gmail.com>
2019-01-30 13:09:44 -0700

    update http library

--- index.js
+++ index.js
@@ -1,45 +1,43 @@
-
 const Nightmare = require('nightmare');
-const rp = require('request-promise');
+const axios = require('axios');
 
-const run = () => {
-  rp('http://gimmeproxy.com/api/getProxy', {
-    qs : {
-      'protocol' : 'http',
-      "user-agent" : true,
+const run = async () => {
+  const { data } = await axios('http://gimmeproxy.com/api/getProxy', {
+    qs: {
+      protocol: 'http',
+      'user-agent': true,
       supportsHttps: true,
-    }
-  })
-  .then(result => {
+    },
+  });
+  const { type, ipPort } = data;
 
-    result = JSON.parse(result);
+  const proxy = `${type}://${ipPort}`;
+  const nightmare = Nightmare({
+    show: true,
+    webPreferences: {
+      plugins: true,
+    },
+    switches: {
+      'ppapi-flash-path':
+        process.platform === 'darwin'
+          ? './PepperFlashPlayer.plugin'
+          : './libpepflashplayer.so',
+      'proxy-server': proxy,
+      'ignore-certificate-errors': true,
+    },
+  });
 
-    const proxy = `${result.type}://${result.ipPort}`;
-    const nightmare = Nightmare({
-      show: true,
-      webPreferences: {
-        plugins: true
-      },
-      switches : {
-        'ppapi-flash-path': process.platform === 'darwin' ? './PepperFlashPlayer.plugin' : './libpepflashplayer.so',
-        'proxy-server': proxy,
-        'ignore-certificate-errors': true
+  const page = nightmare
+    .goto('http://www.twitch.tv/jbozzz')
+    .on('page', console.log.bind(console))
+
+    .run((err, nightmare) => {
+      if (err) {
+        console.error('err', err);
+        page.end();
+        run();
       }
     });
-
-    const page = nightmare
-      .goto('http://www.twitch.tv/jbozzz')
-      .on('page', console.log.bind(console))
-
-      .run((err, nightmare) => {
-        if (err) {
-          console.error("err", err);
-          page.end();
-          run();
-        }
-      });
-
-  });
-}
+};
 
 run();
